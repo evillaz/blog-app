34a9e3787e72b6f1079a56325d27883ed8dfb2d0
evillaz <evillaz123@gmail.com>
2023-06-22 14:25:11 -0500

    Create comment model

---
+++ app/models/comment.rb
@@ -0,0 +1,11 @@
+class Comment < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+  belongs_to :post
+
+  after_save :update_posts_comments_counter
+  after_destroy :update_posts_comments_counter
+
+  def update_posts_comments_counter
+    post.update(comments_counter: post.comments.count)
+  end
+end
