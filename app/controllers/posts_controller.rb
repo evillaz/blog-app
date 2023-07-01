533eb0c48b42a71cf64195aac7ff83558d7c6012
evillaz <evillaz123@gmail.com>
2023-07-01 02:00:20 -0500

    Update post actions to forms

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,28 @@ class PostsController < ApplicationController
     @user = User.find(params[:user_id])
     @post = @user.posts.find(params[:id])
   end
+
+  def new
+    @user = current_user
+    @post = Post.new
+    respond_to do |format|
+      format.html { render :new, locals: { post: @post, user: @user } }
+    end
+  end
+
+  def create
+    @user = current_user
+    @post = @user.posts.new(params.require(:new_post).permit(:title, :text))
+    respond_to do |format|
+      format.html do
+        if @post.save
+          flash[:success] = 'Post saved successfully'
+          redirect_to user_posts_url(@user)
+        else
+          flash.now[:error] = 'Error: Post could not be saved'
+          render :new
+        end
+      end
+    end
+  end
 end
