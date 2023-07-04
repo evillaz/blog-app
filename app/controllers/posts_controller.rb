5e76a759eb58299a4223996ecfca59c783e6d205
evillaz <evillaz123@gmail.com>
2023-07-04 06:11:21 -0500

    Secure n+1 for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,11 @@
 class PostsController < ApplicationController
   def index
-    @user = User.find(params[:user_id])
-    @posts = @user.posts
+    @user = User.includes(:posts).find(params[:user_id])
+    @posts = @user.posts.includes(comments: :author)
   end
 
   def show
-    @user = User.find(params[:user_id])
-    @post = @user.posts.find(params[:id])
+    @post = Post.includes(comments: :author).find(params[:id])
   end
 
   def new
