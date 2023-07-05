15c9586ae6516604f4e23b198ee77e8d781d4171
evillaz <evillaz123@gmail.com>
2023-07-05 04:45:21 -0500

    Add before_action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     @user = User.includes(:posts).find(params[:user_id])
     @posts = @user.posts.includes(comments: :author).paginate(page: params[:page], per_page: 3)
